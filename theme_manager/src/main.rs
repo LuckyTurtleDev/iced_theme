eac98ca8dffb263c787d1dbf4456f0e764df8fd0
Lukas1818 <git@lukas1818.de>
2022-02-23 22:27:59 +0100

    start manager

--- theme_manager/src/main.rs
+++ theme_manager/src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-	println!("Hello, world!");
+use iced::{Container, Element, Length, Sandbox, Settings};
+
+mod activitys;
+use activitys::{Activity, Message};
+
+pub struct Window {
+	activity: Activity,
+	theme: Box<dyn typetest_themes::ApplicationTheme>,
+}
+
+impl Sandbox for Window {
+	type Message = Message;
+
+	fn new() -> Window {
+		Window {
+			theme: typetest_themes::Theme::DefaultDark.into(),
+			activity: Activity::Select,
+		}
+	}
+
+	fn title(&self) -> String {
+		String::from("rusty-vocabulary")
+	}
+
+	fn update(&mut self, message: Self::Message) {}
+
+	fn view(&mut self) -> Element<Self::Message> {
+		let content: Element<Self::Message> = match self.activity {
+			Activity::Select => activitys::select::view(self),
+		};
+		Container::new(content)
+			.width(Length::Fill)
+			.height(Length::Fill)
+			.center_x()
+			.center_y()
+			.style(&typetest_themes::Theme::DefaultDark.into())
+			.into()
+	}
+}
+
+fn main() -> iced::Result {
+	Window::run(Settings::default())
 }
