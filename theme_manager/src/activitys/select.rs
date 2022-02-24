717a5841212190f23225886f4e0c402b7ac49cf9
Lukas1818 <git@lukas1818.de>
2022-02-24 20:49:30 +0100

    use theme

--- theme_manager/src/activitys/select.rs
+++ theme_manager/src/activitys/select.rs
@@ -1,7 +1,7 @@
 use super::Message;
 use iced::{Container, Element, Length, Text};
 
-fn into_tab<'a, T>(content: T) -> Element<'a, Message>
+fn into_tab<'a, T>(win: &mut super::Window, content: T) -> Element<'a, Message>
 where
 	T: Into<Element<'a, Message>>,
 {
@@ -10,11 +10,11 @@ where
 		.height(Length::Fill)
 		.center_x()
 		.center_y()
-		.style(&typetest_themes::Theme::DefaultDark.into())
+		.style(&win.theme)
 		.into()
 }
 
 pub fn view(win: &mut super::Window) -> Element<super::Message> {
 	let content = Text::new("TODO select");
-	into_tab(content)
+	into_tab(win, content)
 }
